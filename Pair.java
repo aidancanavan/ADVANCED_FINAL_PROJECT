f21d3de4d80048361c89469c6823536b2453c7e9
MattLax15 <m07flet@siena.edu>
2019-03-29 18:15:37 -0400

    added java doc to pair class

--- Pair.java
+++ Pair.java
@@ -1,37 +1,55 @@
 
 /**
- * Write a description of class Pair here.
+ * This is a class used to keep track of the number of 
+ * times a specfic string occurs in a given interval
  *
- * @author (your name)
- * @version (a version number or a date)
+ * @author (Team 5)
+ * @version (1.3)
  */
+
+
 public class Pair
 {
     private int count;
     private String c;
-    
+
     public Pair(String c, int count){
-    this.count = count;
-    this.c = c;
-    
+        this.count = count;
+        this.c = c;
+
     }
-    
+
+    /**
+     * this method increments the varible count by 1
+     * 
+     */
     public void add(){
-    count = count + 1;
+        count = count + 1;
     }
-    
+
+    /**
+     * this method returns the current count
+     *  
+     */
     public int getCount(){
-    return count;
+        return count;
     }
+
     
+    /**
+     * this method sets the current pair's string value
+     */
     public void setLetter(String c){
-    this.c = c;
+        this.c = c;
     }
-    
+
+    /**
+     * this method returns the pair's current string value
+     * 
+     * @return the string associated with the pair
+     */
     public String getLetter(){
-    return c;
+        return c;
     }
-    
-    
-    
+
 }
